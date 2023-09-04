dcb926156b897f28f6840b223b50d2e84ecca346
KhalifahMB <muhammadelkufahn27@gmail.com>
2023-09-04 10:14:28 +0100

    initial commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">El-kufahn</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Login</button>
+        </form>
+        <p>You dont hava an account? Register </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
